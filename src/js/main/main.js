dcdfe7b4c262ec5c4f7776fad23d345cd6909488
AkiTDG <156631567+AkiTDG@users.noreply.github.com>
2025-05-10 21:40:42 +0800

    Update main.js

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -145,8 +145,8 @@ writeToConsole(asciitext);
 writeToConsole('+--------------------------------------------------+');
 writeToConsole('|==================================================|');
 writeToConsole('|        =======[Welcome to EverOS!]=======        |');
-writeToConsole('| Today is:   ' + new Date().toLocaleDateString().padEnd(35) + '|');
-writeToConsole('| Time check: ' + new Date().toLocaleTimeString().padEnd(35) + '|');
+writeToConsole('| Today is:   ' + new Date().toLocaleDateString().padEnd(37) + '|');
+writeToConsole('| Time check: ' + new Date().toLocaleTimeString().padEnd(37) + '|');
 writeToConsole('|==================================================|');
 writeToConsole('+--------------------------------------------------+');
 
