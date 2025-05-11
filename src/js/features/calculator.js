64c52aa1f1bcb6e6e279b38ccdea2ddec872299e
AkiTDG <156631567+AkiTDG@users.noreply.github.com>
2025-05-11 15:01:09 +0800

    Update calculator.js

--- src/js/features/calculator.js
+++ src/js/features/calculator.js
@@ -1,24 +1,4 @@
-export function calculator(expression) {
-    try {
-      const cleanExpression = expression.replace(/\s+/g, '');
-
-       if (!/^[0-9+\-*/().%]+$/.test(cleanExpression)) {
-           throw new Error('Invalid characters.');
-        }
-
-        const result = new Function('return '+cleanExpression)();
-        if (result === undefined || isNaN(result)) {
-            throw new Error('Syntax error');
-        }
-        if (!isFinite(result)) {
-            throw new Error('Math error');
-        }
-        writeToConsole(`= ${result}`);
-    } catch (error) {
-        writeToConsole('Statement error');
-    }
-} 
-
+export function calculator(expression){try{const cleanExpression=expression.replace(/\s+/g,"");if(!/^[0-9+\-*/().%]+$/.test(cleanExpression)){throw new Error("Invalid characters.")}const result=new Function("return "+cleanExpression)();if(result===undefined||isNaN(result)){throw new Error("Syntax error")}if(!isFinite(result)){throw new Error("Math error")}writeToConsole(`= ${result}`)}catch(error){writeToConsole("Statement error")}}
 //calc menu
 export const calcUi = `
 +------------------------------------------------------------------------+
