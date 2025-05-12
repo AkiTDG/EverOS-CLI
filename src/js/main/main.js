f0e79dd1ac21baa2995a7706b5a0114aa585c881
AkiTDG <156631567+AkiTDG@users.noreply.github.com>
2025-05-12 19:35:24 +0800

    Added support for msc feature

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -1,1 +1,53 @@
-import{handleCommand}from"./commandHandler.js";import{homeMenu,helpMenu}from"./mainUI.js";import{calculator,calcUi}from"../features/calculator.js";import{temperatureConverter,resetTempMode,tcUi}from"../features/temperature_converter.js";const consoleDiv=document.getElementById("console");const inputField=document.getElementById("input");inputField.focus();let currentFeature="home";function getCurrentFeature(){return currentFeature}function setCurrentFeature(value){currentFeature=value}function writeToConsole(text){const consoleDiv=document.getElementById("console");consoleDiv.textContent+=text+"\n";consoleDiv.scrollTop=consoleDiv.scrollHeight}window.writeToConsole=writeToConsole;inputField.addEventListener("keydown",function(event){if(event.key==="Enter"){const command=inputField.value;if(command.trim()!==""){writeToConsole(">> "+command)}handleCommand(command,{currentFeatureGetter:getCurrentFeature,currentFeatureSetter:setCurrentFeature,writeToConsole:writeToConsole,consoleDiv:consoleDiv,homeMenu:homeMenu(),helpMenu:helpMenu,calculator:calculator,calcUi:calcUi,temperatureConverter:temperatureConverter,tcUi:tcUi,resetTempMode:resetTempMode});inputField.value=""}});writeToConsole(homeMenu());
+import{handleCommand}from "./commandHandler.js"
+import{homeMenu,helpMenu}from "./mainUI.js"
+import{calculator,calcUi}from "../features/calculator.js"
+import{temperatureConverter,resetTempMode,tcUi}from "../features/temperature_converter.js"
+import{measurementConverter,mscUI}from "../features/measurement_system_converter.js"
+
+const consoleDiv = document.getElementById("console")
+const inputField = document.getElementById("input")
+inputField.focus()
+let currentFeature = "home"
+
+function getCurrentFeature()
+{
+	return currentFeature
+}
+
+function setCurrentFeature(value)
+{
+	currentFeature = value
+}
+
+function writeToConsole(text)
+{
+	const consoleDiv = document.getElementById("console")
+	consoleDiv.textContent += text + "\n"
+	consoleDiv.scrollTop = consoleDiv.scrollHeight
+}
+window.writeToConsole = writeToConsole
+inputField.addEventListener("keydown", function (event)
+{
+	if (event.key === "Enter")
+	{
+		const command = inputField.value
+		if (command.trim() !== "")
+		{
+			writeToConsole(">> " + command)
+		}
+		handleCommand(command,
+		{
+		currentFeatureGetter: getCurrentFeature,
+		currentFeatureSetter: setCurrentFeature,
+		writeToConsole: writeToConsole,
+		consoleDiv: consoleDiv,	
+		homeMenu: homeMenu(),
+		helpMenu: helpMenu,
+		calculator: calculator,calcUi: calcUi,
+		temperatureConverter: temperatureConverter,tcUi: tcUi,resetTempMode: resetTempMode,
+		measurementConverter: measurementConverter, mscUI:mscUI
+		})
+		inputField.value = ""
+	}
+})
+writeToConsole(homeMenu())
