a8ae8e9564041930e6090b32682bf32744546964
AkiTDG <156631567+AkiTDG@users.noreply.github.com>
2025-06-15 01:18:24 +0800

    Add files via upload

--- src/js/features/experimental/testASCIIAnimation.js
+++ src/js/features/experimental/testASCIIAnimation.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {frames} from "./animation_framesets/animationFrames.js"
 import {framesetTest} from "./animation_framesets/badapple_framesettest.js"
 let animationInterval;
@@ -65,71 +64,3 @@ export function stopAnimation() {
   }
   document.removeEventListener("keydown", stopHandlerKey);
 }
-=======
-import {frames} from "./animation_framesets/animationFrames.js"
-import {framesetTest} from "./animation_framesets/badapple_framesettest.js"
-let animationInterval;
-let currentSetIndex = 0;
-let currentFrameIndex = 0;
-let startTimeout;
-const animationSets = [frames,framesetTest];
-
-export function runAnimation() {
-  const animationLine = document.getElementById("console");
-  if (animationLine) animationLine.textContent = "Loading ASCII animation...";
-
-  currentSetIndex = 0;
-  currentFrameIndex = 0;
-
-  startTimeout = setTimeout(() => {
-  playNextFrame();
-  document.addEventListener("keydown", stopHandlerKey);
-  }, 3000);
-  if (startTimeout || animationInterval) return;
-}
-
-function playNextFrame() {
-  const animationLine = document.getElementById("console");
-  if (!animationLine || currentSetIndex >= animationSets.length) {
-    stopAnimation();
-    return;
-  }
-
-  const currentFrames = animationSets[currentSetIndex];
-  animationLine.textContent = currentFrames[currentFrameIndex];
-  currentFrameIndex++;
-
-  if (currentFrameIndex >= currentFrames.length) {
-    currentSetIndex++;
-    currentFrameIndex = 0;
-    animationInterval = setTimeout(playNextFrame, 49); 
-  } else {
-    animationInterval = setTimeout(playNextFrame, 49); 
-  }
-}
-
-export function stopHandlerKey(e) {
-  if (["End", "Home", "Delete"].includes(e.key)) {
-    stopAnimation();
-  }
-}
-
-export function stopAnimation() {
-  if (!startTimeout && !animationInterval) return;
-  if (startTimeout) {
-    clearTimeout(startTimeout);
-    startTimeout = null;
-  }
-  if (animationInterval) {
-    clearTimeout(animationInterval);
-    animationInterval = null;
-  }
-  currentSetIndex = 0;
-  currentFrameIndex = 0;
-  const animationLine = document.getElementById("console");
-  if (animationLine) {
-    animationLine.textContent = "Animation stopped."
-  }
-  document.removeEventListener("keydown", stopHandlerKey);
-}
->>>>>>> a9c77a03f4d37210db88dbe058f2ac3395318559
